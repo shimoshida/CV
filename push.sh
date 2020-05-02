c8a2eade106524bba9a62637e58b0446fc3e11ff
shimoda masayuki <shimoda.m.af@m.titech.ac.jp>
2020-05-02 21:30:38 +0900

    kf

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 git config --global user.email "8169618+shimoshida@users.noreply.github.com"
+git config --global user.name "moshida"
+
 git clone https://github.com/shimoshida/CV.git
 cd CV
 git checkout pdf
